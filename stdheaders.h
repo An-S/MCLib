c358f5d6771cbc01530cdda03eefa29d54c016d9
SA <mc78@outlook.de>
2016-04-24 22:31:13 +0200

    added files which would be overwritten by merge

---
+++ stdheaders.h
@@ -0,0 +1,20 @@
+#ifndef STD_HEAD_H
+#define STD_HEAD_H
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <time.h>
+
+#include <conio.h>
+
+#endif
+
