01b7943ccf19b6aa834a48e56a12ddabdf444e1c
mc78 <mc78@outlook.de>
2016-06-19 14:05:21 +0200

    added possibility to repeat sinus table after each other

--- trigFunctions/trigFunctions.h
+++ trigFunctions/trigFunctions.h
@@ -7,6 +7,7 @@
 typedef int MCLib_sinelem_t;
 typedef struct{
     size_t len;
+    size_t size;
     MCLib_sinelem_t min;
     MCLib_sinelem_t max;
 } MCLib_sinparameters_t;
@@ -18,7 +19,22 @@ typedef struct {
 } MCLib_sintable_t;
 
 
-MCLib_sintable_t *MCLib_createSin(size_t len, MCLib_sinelem_t min, MCLib_sinelem_t max);
+/** \brief calculates sintable with specified min, max and length. The parameter N specifies how many times
+ *          the sin is repeated
+ * \param MCLib_sinelem_t min = minimum value of sintable entries
+ * \param MCLib_sinelem_t max = maximum value of sintable entries
+ * \param size_t len = length of one period in bytes
+ * \param size_t N = number of repetitions
+ * \return MCLib_sintable_t * = pointer to sinus table data structure
+ *
+ */
+MCLib_sintable_t *MCLib_createSin(size_t len, MCLib_sinelem_t min, MCLib_sinelem_t max, size_t N);
+
+/** \brief destoys sinus table which was allocated and calculated by MCLib_createSin
+ * \param MCLib_sintable_t * = pointer to sinus table data structure
+ * \return int = success or failure
+ *
+ */
 int MCLib_destroySin(MCLib_sintable_t  *sintable);
 
 #endif // TRIGFUNCTIONS_H_INCLUDED
